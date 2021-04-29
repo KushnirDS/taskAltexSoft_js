3c4ecb256db635a3e3dc6840032bd73f7e28ab3a
KushnirDS <d.s.kushnir@gmail.com>
2021-04-29 13:10:06 +0300

    fix output

--- taskAltexSoftFin.js
+++ taskAltexSoftFin.js
@@ -3,11 +3,21 @@ let oneLetterNameClient;
 
 let select = document.getElementById('arrLetter');
 let buttonSearch = document.getElementById('buttonSearch');
+let list = document.getElementById('listName');
 
 function buttonClick() {
     let selectValue = select.options[select.selectedIndex].value;
     mySearch(selectValue);
 }
+
+function addListName(name) {
+        let content = name;
+        let addElement = document.createElement('li');
+        addElement.textContent = content;
+        list.appendChild(addElement);
+}
+
+
 
 function pushHTMLSelect() {
     for (let i = 0; i < arrletter.length; i++) {
@@ -40,7 +50,8 @@ function mySearch(valueOfSelect) {
     for (let i = 0; i < clients.length; i++) {
         oneLetterNameClient = clients[i].name[0];
         if (oneLetterNameClient === valueOfSelect) {
-            window.alert('Id:' + ' ' + clients[i]._id + ' ' + 'Name:' + ' ' + clients[i].name);
+            addListName(clients[i].name);
+            // window.alert('Id:' + ' ' + clients[i]._id + ' ' + 'Name:' + ' ' + clients[i].name);
             notFound = true
         } 
     }
