b96d7b4c2785ec4c18215a3c11cb8ff41f5ca449
KushnirDS <d.s.kushnir@gmail.com>
2021-05-05 10:43:42 +0300

    fix code

--- taskAltexSoftFin.js
+++ taskAltexSoftFin.js
@@ -2,21 +2,21 @@ let arrletter = [];
 let oneLetterNameClient;
 
 let select = document.getElementById('arrLetter');
-let buttonSearch = document.getElementById('buttonSearch');
-
 let list = document.getElementById('listName');
 
 function buttonClick() {
+    let element = document.querySelectorAll('#listName li');
+    for (let i = 0; i < element.length; i++) {
+    element[i].parentNode.removeChild(element[i]);
+    }
+
     let selectValue = select.options[select.selectedIndex].value;
     mySearch(selectValue);
     
 }
 
 function addListName(name) {
-    let element = document.querySelectorAll('#listName li');
-    for (let i = 0; i < element.length; i++) {
-        element[i].parentNode.removeChild(element[i]);
-    }    
+    
     let content = name;
     let addElement = document.createElement('li');
     addElement.textContent = content;
@@ -42,7 +42,7 @@ function myRandom(min, max) {
 
 
 //push the array with letters  
-for (let i = 0; i <= 4; i++) {
+for (let i = 0; i < 5; i++) {
     arrletter.push(String.fromCharCode(myRandom(97,122)).toUpperCase());
 }
 
@@ -57,7 +57,6 @@ function mySearch(valueOfSelect) {
         oneLetterNameClient = clients[i].name[0];
         if (oneLetterNameClient === valueOfSelect) {
             addListName(clients[i].name);
-            // window.alert('Id:' + ' ' + clients[i]._id + ' ' + 'Name:' + ' ' + clients[i].name);
             notFound = true
         } 
     }
@@ -68,11 +67,6 @@ function mySearch(valueOfSelect) {
 
 
 
-// window.alert('Id:' + ' ' + clients[1]._id + ' ' + 'Name:' + ' ' + clients[1].name)
-pushHTMLSelect();
-// buttonSearch.addEventListener('click', buttonClick);
-// mySearch (select.options[select.selectedIndex].value);
 
-// let searchElement = select.options.selectedIndex;
-// let txt = select.options[searchElement].text;
-// console.log(txt);
+pushHTMLSelect();
+
